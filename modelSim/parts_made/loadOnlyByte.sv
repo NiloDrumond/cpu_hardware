0817c676a457e97e0af3ab21208f1f4381eec042
unknown <didiermaranhao@gmail.com>
2021-03-24 10:39:12 -0300

    .sv deletados

--- modelSim/parts_made/loadOnlyByte.sv
+++
@@ -1,9 +0,0 @@
-module loadOnlyByte(
-	input logic [31:0] adress,
-	output logic [31:0] result
-);
-
-always
-	result = {24'b0, adress[7:0]};
-	
-endmodule: loadOnlyByte
--- modelSim/parts_made/shiftLeft16.sv
+++
@@ -1,8 +0,0 @@
-module shiftLeft16(
-input logic [31:0] immediate,
-output logic [31:0] shiftLeft16Out
-);
-
-always @ (*) shiftLeft16Out = immediate << 16;
-	
-endmodule
