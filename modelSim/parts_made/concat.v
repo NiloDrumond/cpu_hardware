915f41d6c30a4864a91dbb2846049ba20080153d
Gabriel Meireles <gasm@cin.ufpe.br>
2021-03-24 17:05:01 -0300

    concat consertado e waveform

--- modelSim/parts_made/concat.v
+++ modelSim/parts_made/concat.v
@@ -1,13 +1,13 @@
 module concat(
-input wire [31:0] pcin,
-input wire [4:0] inst25_21,
-input wire [4:0] inst20_16,
-input wire [15:0] inst15_0,
-output reg [31:0] saida
+    input wire [31:0] PC_out,
+    input wire [4:0] inst25_21,
+    input wire [4:0] inst20_16,
+    input wire [15:0] inst15_0,
+    output reg [31:0] saida
 );
 
-logic [27:0] aux20_16;
-logic [27:0] aux25_21;
+reg [27:0] aux20_16;
+reg [27:0] aux25_21;
 
 always @ (*) begin
 	aux20_16 = (inst20_16 << 16);
@@ -15,10 +15,10 @@ always @ (*) begin
 	saida = (32'b00000000000000000000000000000000 + inst15_0);
 	saida = (saida + aux20_16 + aux25_21);
 	saida = (saida << 2);
-	saida[31] = (pcin[31]);
-	saida[30] = (pcin[30]);
-	saida[29] = (pcin[29]);
-	saida[28] = (pcin[28]);
+	saida[31] = (PC_out[31]);
+	saida[30] = (PC_out[30]);
+	saida[29] = (PC_out[29]);
+	saida[28] = (PC_out[28]);
 end		
 
 endmodule
