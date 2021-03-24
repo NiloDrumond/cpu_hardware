8bb41e593911b2110fc8a9172a3b40c3597a243a
Gabriel Meireles <gasm@cin.ufpe.br>
2021-03-24 12:14:26 -0300

    waveform sign_xtend_1_to_32

--- modelSim/parts_made/sign_xtend_1_to_32.v
+++ modelSim/parts_made/sign_xtend_1_to_32.v
@@ -1,6 +1,8 @@
 module sign_xtend_1_to_32(
     input wire Data_in,
-    output wire [31:0] Data_out
+    output reg [31:0] Data_out
 );
-   Data_out = <= (32'b00000000000000000000000000000000 + Data_in);
+always@(*)begin
+    Data_out = (32'b00000000000000000000000000000000 + Data_in);
+end
 endmodule
