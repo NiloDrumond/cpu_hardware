8f861e1460403cd383eb25f099304869a3fc4fe0
Gabriel Meireles <gasm@cin.ufpe.br>
2021-03-24 10:52:29 -0300

    imagem e mux_aluScrB consertado

--- modelSim/parts_made/mux_aluSrcB.v
+++ modelSim/parts_made/mux_aluSrcB.v
@@ -1,17 +1,18 @@
-module mux_ulaB(
+module mux_aluSrcB(
     input wire [1:0]seletor,
     input wire [31:0]B_out,
     input wire [31:0]sign16_to_32,
     input wire [31:0]shiftLeft2,
-    output wire [31:0]Data_out
+    output reg [31:0]Data_out
 );
 
-always 
-	case (seletor)
+always@(*)begin
+   case (seletor)
 		2'd0:Data_out = B_out;
 		2'd1:Data_out = 32'd4;
 		2'd2:Data_out = sign16_to_32;
         2'd3:Data_out = shiftLeft2;
-	endcase
+	endcase 
+end
 	
 endmodule
